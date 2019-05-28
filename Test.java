bdcd47d573bb817a56045ecf0f5432b21719814d
vishal <vishal@pioctave.com>
2019-05-28 23:23:44 +0530

    First

---
+++ Test.java
@@ -0,0 +1,1 @@
+public class Test{	public static void main(String args[])	{		for (int i=65;i<=69 ;i++ ) {			for (int j=65;j<=i ;j++ ) {			System.out.print(j+" ");					}	System.out.println("");		}	}}
