6a568f43af7190a2f5a7f491ddc649ddc1959cad
vishal <vishal@pioctave.com>
2019-05-29 11:20:25 +0530

    add jaggedarray algo

---
+++ JaggedArray.java
@@ -0,0 +1,1 @@
+public class JaggedArray{	/*printing jagged array int number1 2 3 3 4 5 	*/ 	public static void main(String args[])	{		int a[][]=new int[3][];	    a[0]=new int[1];	    a[1]=new int[2];		a[2]=new int[3];for (int i=0;i<a.length ;i++ ) {		for (int j=0;j<a[i].length ;j++ ) {			a[i][j]=i+j+1;			System.out.print(a[i][j]+" ");		}			System.out.println("");		}	}}
